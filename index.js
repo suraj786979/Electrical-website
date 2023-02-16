23e2f12c5ff1004c076fe3aaf3e0b477d3662fa0
cod with Suraj <sp887000@gmail.com>
2023-02-16 11:33:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper', {
+
+    autoplay: {
+      delay: 3000,
+      disableonInteraction: false
+    },
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
---
+++ responsive.js
@@ -0,0 +1,6 @@
+burger = document.querySelector('.burger')
+nav = document.querySelector('.nav')
+
+burger.addEventListener('click', ()=>{
+    nav.classList.toggle('o-class')
+})
